0e956f6300358eac5c8743af9cedd03a6d882906
notashwinii <ashwinii.subedi@gmail.com>
2024-07-18 17:17:52 +0545

    router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 
+import React from "react";
+import {Routes, Route } from "react-router-dom";
+import {Editor} from "./components";
+const App=() =>{
 
-function App() {
-  return "Hi"
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <Routes>
+        <Route path= "/editor/*" element={<Editor/>}/>
+      </Routes>
+      
+    </div>
+  )
 }
 
 export default App;
